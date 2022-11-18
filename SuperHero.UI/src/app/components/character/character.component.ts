4246512d355e5d2f2dd99a675a77e54f7660e679
Petri Eskelinen <eskpetri@gmail.com>
2022-11-18 17:54:28 +0200

    No coodi oli aikaisemmin oikein, mutta conf...

--- SuperHero.UI/src/app/components/character/character.component.ts
+++ SuperHero.UI/src/app/components/character/character.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Character } from 'src/app/models/character';
+import { CharacterService } from 'src/app/services/characters.service';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
   styleUrls: ['./character.component.css']
 })
-export class CharacterComponent implements OnInit {
+export class EditcharacterComponent implements OnInit {
+  @Input() character?: Character;
+  @Output() charactersUpdated = new EventEmitter<Character[]>();
 
-  constructor() { }
+  constructor(private CharacterService: CharacterService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  updateCharacter(character: Character) {
+    this.CharacterService
+      .updateCharacter(character)
+      .subscribe((characteres: Character[]) => this.charactersUpdated.emit(characteres));
   }
 
+  deleteCharacter(character: Character) {
+    this.CharacterService
+      .deleteCharacter(character)
+      .subscribe((characteres: Character[]) => this.charactersUpdated.emit(characteres));
+  }
+
+  createCharacter(character: Character) {
+    this.CharacterService
+      .createCharacter(character)
+      .subscribe((characteres: Character[]) => this.charactersUpdated.emit(characteres));
+  }
 }
+
