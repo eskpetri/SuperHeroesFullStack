9ed59d43782d0ad2f06b85cf735188d60d866f7a
Petri Eskelinen <eskpetri@gmail.com>
2022-11-18 15:12:49 +0200

    Basics done

--- SuperHero.UI/src/app/app.component.ts
+++ SuperHero.UI/src/app/app.component.ts
@@ -1,23 +1,31 @@
 import { Component } from '@angular/core';
 import { SuperHero } from './models/super-hero';
 import { SuperHeroService } from './services/super-hero.service';
+import { Character } from './models/character';
+import { CharacterService } from './services/characters.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'SuperHero.UI';
   heroes: SuperHero[] = [];
   heroToEdit?: SuperHero;
+  characters: Character[] = [];
+  characterToEdit?: Character;
 
-  constructor(private superHeroService: SuperHeroService) {}
+  constructor(private superHeroService: SuperHeroService, private characterService: CharacterService) {}
 
   ngOnInit(): void {
     this.superHeroService
       .getSuperHeroes()
       .subscribe((result: SuperHero[]) => (this.heroes = result));
+    this.characterService
+      .getCharacters()
+      .subscribe((result: Character[]) => (this.characters = result));
   }
 
   updateHeroList(heroes: SuperHero[]) {
@@ -31,4 +39,16 @@ export class AppComponent {
   editHero(hero: SuperHero) {
     this.heroToEdit = hero;
   }
+  updateCharacterList(heroes: SuperHero[]) {
+    this.heroes = heroes;
+  }
+
+  initNewCharacter() {
+    this.heroToEdit = new SuperHero();
+  }
+
+  editCharacter(hero: SuperHero) {
+    this.heroToEdit = hero;
+  }
+
 }
