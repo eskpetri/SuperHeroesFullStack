d1b3aed14bc7c7ea19af2e697e8448ef1d6f9bea
Petri Eskelinen <eskpetri@gmail.com>
2022-11-19 16:07:40 +0200

    CSS fixaus Lines in order

--- SuperHero.UI/src/app/app.module.ts
+++ SuperHero.UI/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditHeroComponent } from './components/edit-hero/edit-hero.component';
-import { FormsModule } from '@angular/forms';
-import { EditcharacterComponent } from './components/character/character.component';
+import { EditCharacterComponent } from './components/character/character.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EditHeroComponent,
-    EditcharacterComponent
+    EditCharacterComponent
   ],
   imports: [
     BrowserModule,
